f779199a583f40ba762f89efad9a59bf16b5988b
melokuhlemazii <melomazibuko8@gmail.com>
2026-05-12 19:28:21 +0200

    Created Home Page

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,4 +1,26 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
 public class HomePage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//span[text()='Login']")
+    WebElement loginHeading_xpath;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickLoginButton(){
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginHeading_xpath));
+        loginHeading_xpath.isDisplayed();
+    }
 }
